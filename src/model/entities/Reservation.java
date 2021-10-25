474892bd963ff3959508d0ac32386c97da20d8eb
Madu Sventnickas <eduardasventnickas@gmail.com>
2021-10-25 16:03:39 -0300

    Projeto Tratamento Concluido

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 	
 	private Integer roomNumber;
@@ -13,6 +15,13 @@ public class Reservation {
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	
 	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if (checkIn.before(now)|| checkOut.before(now)){
+			throw new DomainException("Reservation dates must be futere dates.");
+		}
+		else if(!checkOut.after(checkIn)){
+			throw new DomainException("Check-out date must be after check-in date.");
+		}
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -43,10 +52,10 @@ public class Reservation {
 		
 		Date now = new Date();
 		if (checkIn.before(now)|| checkOut.before(now)){
-			throw new IllegalArgumentException("Reservation dates for update must be futere dates.");
+			throw new DomainException("Reservation dates for update must be futere dates.");
 		}
 		else if(!checkOut.after(checkIn)){
-			throw new IllegalArgumentException("Check-out date must be after check-in date.");
+			throw new DomainException("Check-out date must be after check-in date.");
 		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
